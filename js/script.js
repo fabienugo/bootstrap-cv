1cda00fe706eb31daff68ec7474aa0bb67b1fc52
Fabien Ugo <fabien.ugo@hotmail.fr>
2019-06-27 20:12:56 +0200

    Projet bootstrap cv

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* global document $ window */
+
+$(document).ready(function(){
+    
+    $(".navbar a, footer a").on("click"), function(event){
+        
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body').animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
+    };
+});
